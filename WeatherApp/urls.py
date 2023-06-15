2d1040ec55fb7b4161fb581f6dcc3d4321a68826
ElenaGlu <shishalovae@mail.ru>
2023-06-15 17:54:09 +0500

    configured templates

--- WeatherApp/urls.py
+++ WeatherApp/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from Aap import views
+from App import views
 
 urlpatterns = [
     path('', views.weather_forecast, name='weather_forecast'),
