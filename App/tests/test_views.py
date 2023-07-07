de633ce8d9ca047e4b062456b404fca539532619
ElenaGlu <shishalovae@mail.ru>
2023-07-07 19:12:05 +0500

    add pytest

--- App/tests/test_views.py
+++ App/tests/test_views.py
@@ -1,2 +1,10 @@
-from django.test import TestCase
+import pytest
 
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_view(client):
+    url = reverse('homepage-url')
+    response = client.get(url)
+    assert response.status_code == 200
