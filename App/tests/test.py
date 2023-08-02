8f6f74657d99499498fff41295bf773ebc709fe3
ElenaGlu <shishalovae@mail.ru>
2023-08-02 19:06:30 +0500

    update tests

--- App/tests/test.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from django.test import Client
-
-class ProjectTests(TestCase):
-
-    def test_homepage(self):
-        c = Client()
-        response = c.get('/')
-        self.assertEqual(response.status_code, 200)
