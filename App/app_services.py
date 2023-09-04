eb937e6bf7f49763d771066a10cb248b28f58708
ElenaGlu <shishalovae@mail.ru>
2023-09-04 16:08:31 +0500

    start 4.09

--- App/app_services.py
+++ App/app_services.py
@@ -24,14 +24,14 @@ def get_forecast_for_today(city: str) -> dict:
     request_for_today = request_to_api_forecast(f'https://api.openweathermap.org/data/2.5/weather?q={city}&units'
                                                 f'=metric&lang=ru&appid={API_KEY_WEATHER}')
 
-    forecast_for_today = {'city': city, 'conditions': str(request_for_today['weather'][0]['description']),
-                          'temperature': str(round(request_for_today['main']['temp'])),
-                          'temperature_feels': str(round(request_for_today['main']['feels_like'])),
-                          'clouds': str(round(request_for_today['clouds']['all'])),
-                          'pressure': str(round((request_for_today['main']['pressure']) / 1.333)),
-                          'humidity': str(round(request_for_today['main']['humidity'])),
-                          'visibility': str(round(request_for_today['visibility'])),
-                          'wind_speed': str(round(request_for_today['wind']['speed'])),
+    forecast_for_today = {'city': city, 'conditions': request_for_today['weather'][0]['description'],
+                          'temperature': round(request_for_today['main']['temp']),
+                          'temperature_feels': round(request_for_today['main']['feels_like']),
+                          'clouds': round(request_for_today['clouds']['all']),
+                          'pressure': round((request_for_today['main']['pressure']) / 1.333),
+                          'humidity': round(request_for_today['main']['humidity']),
+                          'visibility': round(request_for_today['visibility']),
+                          'wind_speed': round(request_for_today['wind']['speed']),
                           'sunrise_timestamp': datetime.datetime.fromtimestamp(request_for_today["sys"]["sunrise"]),
                           'sunset_timestamp': datetime.datetime.fromtimestamp(request_for_today["sys"]["sunset"])}
     return forecast_for_today
@@ -49,8 +49,8 @@ def get_forecast_for_five_days(city: str) -> dict:
     for part_day in range(len(request_for_five_days['list'])):
         one_full_day = (str(request_for_five_days['list'][part_day]['dt_txt']))[:11]
         weather_description = str(request_for_five_days['list'][part_day]['weather'][0]['description'])
-        temp_min = int(str(str(round(request_for_five_days['list'][part_day]['main']['temp_min']))))
-        temp_max = int(str(str(round(request_for_five_days['list'][part_day]['main']['temp_max']))))
+        temp_min = round(request_for_five_days['list'][part_day]['main']['temp_min'])
+        temp_max = round(request_for_five_days['list'][part_day]['main']['temp_max'])
 
         if one_full_day not in forecast_for_five_days.keys():
             forecast_for_five_days[one_full_day] = [100, -100, '']
--- App/tests/test.py
+++ App/tests/test.py
@@ -2,62 +2,36 @@ from django.test import SimpleTestCase
 from django.urls import reverse
 from django.test import Client
 
-import requests
 from config import API_KEY_WEATHER
 
-from jsonschema import validate
-from cerberus import Validator
-from assertpy import assert_that
+import requests
+from json_checker import Checker
 
 
 def test_get_forecast_for_today():
-    schema = {
-        'coord': {'type': 'dict'},
-        'weather': {'id': {'type': 'integer'},
-                    'main': {'type': 'string'},
-                    'description': {'type': 'string'},
-                    'icon': {'type': 'string'}
-                    },
-        'base': {'type': 'string'},
-        'main': {'temp': {'type': 'integer'},
-                 'feels_like': {'type': 'integer'},
-                 'temp_min': {'type': 'integer'},
-                 'temp_max': {'type': 'integer'},
-                 'pressure': {'type': 'integer'},
-                 'humidity': {'type': 'integer'},
-                 'sea_level': {'type': 'integer'},
-                 'grnd_level': {'type': 'integer'}
-                 },
-        'visibility': {'type': 'integer'},
-        'wind': {'speed': {'type': 'integer'},
-                 'deg': {'type': 'integer'},
-                 'gust': {'type': 'integer'}
-                 },
-        'clouds': {'all': {'type': 'integer'}
-                   },
-        'dt': {'type': 'integer'},
-        'sys': {'type': {'type': 'integer'},
-                'id': {'type': 'integer'},
-                'country': {'type': 'string'},
-                'sunrise': {'type': 'integer'},
-                'sunset': {'type': 'integer'}},
-        'timezone': {'type': 'integer'},
-        'id': {'type': 'integer'},
-        'name': {'type': 'string'},
-        'cod': {'type': 'integer'}
-    }
-
     response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q=Москва&units'
                             f'=metric&lang=ru&appid={API_KEY_WEATHER}')
     assert response.headers['Content-Type'] == "application/json; charset=utf-8"
 
-    # assert validate(response.json(), schema) is None
+    schema = {
+        'city': str,
+        'conditions': str,
+        'temperature': int,
+        'temperature_feels': int,
+        'clouds': int,
+        'pressure': int,
+        'humidity': int,
+        'visibility': int,
+        'wind_speed': int,
+    }
 
-    # v = Validator(schema)
-    # validate_response = v.validate(response.json())
-    # assert_that(validate_response, description=v.errors).is_true()
+    dict1 = {'city': 'Москва', 'conditions': 'пасмурно', 'temperature': 22, 'temperature_feels': 21, 'clouds': 95,
+             'pressure': 766, 'humidity': 52, 'visibility': 10000, 'wind_speed': 5}
 
+    checker = Checker(schema)
+    result = checker.validate(dict1)
 
+    assert result == dict1
 
 
 class WeatherForecastTests(SimpleTestCase):
@@ -71,8 +45,3 @@ class WeatherForecastTests(SimpleTestCase):
         response = c.post(reverse("displays_weather_forecast_in_the_city"), {'city': 'Москва'})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "App/weather_forecast.html")
-
-# def test_request_to_api_forecast():
-#     content = json.loads(response.content)
-#     assert 'result' in content
-#     assert content['result'] == 'ok'
