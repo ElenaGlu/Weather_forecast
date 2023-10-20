aec0a1c993c346746f5dfbf73539361f3c6a93c9
ElenaGlu <shishalovae@mail.ru>
2023-10-20 17:50:17 +0500

    need fix bags

--- App/tests/test.py
+++ App/tests/test.py
@@ -1,27 +1,38 @@
-from django.test import SimpleTestCase
+import unittest
+
 from django.urls import reverse
 from django.test import Client
 
-from App.app_services import ForecastWeather
-
 from jsonschema import validate
 
+from App.app_services import WeatherForecast
 
-class WeatherForecastTests(SimpleTestCase):
+
+class WeatherForecastTests(unittest.TestCase):
 
     def test_search_page(self):
-        response = self.client.get(reverse("loading_search_page"))
+        """
+        Requests a html page and checks its status code and the correctness of the template
+        """
+        c = Client()
+        response = c.get(reverse("loading_search_page"))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "App/base.html")
+        self.assertEqual(response, "App/base.html")
 
     def test_city_page(self):
+        """
+        Requests a html page with city and checks its status code and the correctness of the template
+        """
         c = Client()
         response = c.post(reverse("displays_weather_forecast_in_the_city"), {'city': 'Москва'})
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "App/weather_forecast.html")
+        self.assertEqual(response, "App/weather_forecast.html")
 
     @staticmethod
-    def test_get_forecast_for_today():
+    def test_data_processing_for_today():
+        """
+        Validation of the json schema in the forecast for today
+        """
         schema = {
             'type': 'object',
             'required': ['city', 'conditions', 'temperature',
@@ -64,12 +75,23 @@ class WeatherForecastTests(SimpleTestCase):
                 },
             }
         }
-
-        response = ForecastWeather('Москва')
+        forecast = WeatherForecast('Москва')
+        json = {'coord': {'lon': 37.6156, 'lat': 55.7522},
+                'weather': [{'id': 804, 'main': 'Clouds', 'description': 'пасмурно', 'icon': '04d'}],
+                'base': 'stations',
+                'main': {'temp': 5.16, 'feels_like': 2.24, 'temp_min': 3.94, 'temp_max': 6.29, 'pressure': 1011,
+                         'humidity': 77, 'sea_level': 1011, 'grnd_level': 993}, 'visibility': 10000,
+                'wind': {'speed': 3.69, 'deg': 76, 'gust': 4.83}, 'clouds': {'all': 100}, 'dt': 1697796218,
+                'sys': {'type': 2, 'id': 2000314, 'country': 'RU', 'sunrise': 1697774956, 'sunset': 1697811554},
+                'timezone': 10800, 'id': 524901, 'name': 'Москва', 'cod': 200}
+        response = forecast.data_processing_for_today(json)
         assert not validate(response, schema)
 
     @staticmethod
-    def test_get_forecast_for_five_days():
+    def test_data_processing_for_five_days():
+        """
+        Validation of the json schema in the forecast for five days
+        """
         schema = {
             'type': 'object',
             'required': ['day', 'day_1', 'day_2', 'day_3', 'day_4', 'day_5'],
@@ -107,5 +129,7 @@ class WeatherForecastTests(SimpleTestCase):
             }
         }
 
-        response = ForecastWeather('Москва')
+        json = ''
+        forecast = WeatherForecast('Москва')
+        response = forecast.data_processing_for_five_days(json)
         assert not validate(response, schema)
