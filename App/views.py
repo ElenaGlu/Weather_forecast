671b1608246a228a169075628e5802e987865e26
ElenaGlu <shishalovae@mail.ru>
2023-10-24 14:30:14 +0500

    ok

--- App/views.py
+++ App/views.py
@@ -2,16 +2,15 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
+from config import API_KEY_WEATHER
 from .app_services import WeatherForecast
 from .forms import PlaceForm
-
-from config import API_KEY_WEATHER
 
 
 def loading_search_page(request: HttpRequest) -> HttpResponse:
     """
     Loads the starting html page.
-    :param request: http request
+    param request: http request
     :return: HTML page with string 'search city'
     """
     return render(request, 'App/base.html')
@@ -20,15 +19,15 @@ def loading_search_page(request: HttpRequest) -> HttpResponse:
 def displays_weather_forecast_in_the_city(request: HttpRequest) -> HttpResponse:
     """
     Displays the weather forecast for the city request from the form.
-    :param request: http request
+    param request: http request
     :return: HTML page with forecast for today and for five day
     """
     if request.method == 'POST':
         form = PlaceForm(request.POST)
         if form.is_valid():
             city = form.cleaned_data["city"]
-            forecast = WeatherForecast(city)
 
+            forecast = WeatherForecast(city)
             forecast_for_today = forecast.choose_forecast(
                 f'https://api.openweathermap.org/data/2.5/weather?q={forecast.city}&units'
                 f'=metric&lang=ru&appid={API_KEY_WEATHER}', forecast.data_processing_for_today)
@@ -39,6 +38,5 @@ def displays_weather_forecast_in_the_city(request: HttpRequest) -> HttpResponse:
 
             return render(request, 'App/weather_forecast.html', {'forecast_for_today': forecast_for_today,
                                                                  'forecast_for_five_days': forecast_for_five_days})
-
         raise ValueError()
 
